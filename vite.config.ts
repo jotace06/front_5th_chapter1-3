81aeafe7c84db916890ba1c6bdea1ab4bea2079d
JC Chung <jc@JCs-MacBook-Pro.local>
2025-04-10 16:04:30 +0900

    배포 관련 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,26 @@ import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default mergeConfig(
-  defineConfig({
+function createBaseConfig(command: "build" | "serve") {
+  return {
+    base: command === "build" ? "/front_5th_chapter1-3/" : "/",
     plugins: [react()],
-  }),
-  defineTestConfig({
-    test: {
-      globals: true,
-      environment: "jsdom",
-      setupFiles: "./src/setupTests.ts",
-      coverage: {
-        reportsDirectory: "./.coverage",
-        reporter: ["lcov", "json", "json-summary"],
-      },
+  };
+}
+
+const testConfig = defineTestConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    coverage: {
+      reportsDirectory: "./.coverage",
+      reporter: ["lcov", "json", "json-summary"],
     },
-  }),
-);
+  },
+});
+
+export default defineConfig(({ command }) => {
+  const baseConfig = createBaseConfig(command);
+  return mergeConfig(baseConfig, testConfig);
+});
